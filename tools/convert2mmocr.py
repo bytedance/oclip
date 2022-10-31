e2bd12bc6295cec6ebcc2f1c3c7dba3d41c36f66
Wenqing Zhang <wenqingzhang@bytedance.com>
2022-10-31 22:12:10 +0800

    update stem index to support mmocr clip_resnet

--- tools/convert2mmocr.py
+++ tools/convert2mmocr.py
@@ -10,20 +10,20 @@ def convert_param_name(model_path, save_path):
             new_k = k[len('module.visual.'):]
             # update parameter name in stem layers
             if new_k.startswith('conv1'):
-                new_k =  new_k.replace('conv1', 'stem.0')
+                new_k = new_k.replace('conv1', 'stem.0')
             elif new_k.startswith('bn1'):
-                new_k =  new_k.replace('bn1', 'stem.1')
+                new_k = new_k.replace('bn1', 'stem.1')
             elif new_k.startswith('conv2'):
-                new_k =  new_k.replace('conv2', 'stem.2')
+                new_k = new_k.replace('conv2', 'stem.3')
             elif new_k.startswith('bn2'):
-                new_k =  new_k.replace('bn2', 'stem.3')
+                new_k = new_k.replace('bn2', 'stem.4')
             elif new_k.startswith('conv3'):
-                new_k =  new_k.replace('conv3', 'stem.4')
+                new_k = new_k.replace('conv3', 'stem.6')
             elif new_k.startswith('bn3'):
-                new_k =  new_k.replace('bn3', 'stem.5')
+                new_k = new_k.replace('bn3', 'stem.7')
             
             # update parameter name in bottleneck blocks
-            new_k =  new_k.replace('downsample.1', 'downsample.2').replace('downsample.0', 'downsample.1')
+            new_k = new_k.replace('downsample.1', 'downsample.2').replace('downsample.0', 'downsample.1')
 
             print(new_k, v.shape)
             backbone_dict[new_k] = v
